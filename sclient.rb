86f6a75debd5ca9a8fa0e79ebb0d0a684bd2fa6e
申思维 <shensiwei@siwei.tech>
2017-11-24 10:14:07 +0800

    可以打印中文了

--- sclient.rb
+++ sclient.rb
@@ -3,30 +3,26 @@ puts "=== before require "
 require 'socket'
 
 sleep 0.1
-puts "=== 111"
 client = TCPSocket.new '192.168.1.181', 9100
-
 sleep 0.1
-puts "=== 222 "
-# 加粗
-client.write "\x1B\x21\x08  bold ..."
-puts "=== 333"
-# 倍高
-client.write "\x1B\x21\x10  2x height..."
-puts "=== 444"
-# 倍宽
-client.write "\x1B\x21\x20  2x width...\n"
 
-puts "=== 555"
+
+
+
 # 倍宽, 倍高
-client.write "\x1B\x21\x30 2x width, height..."
+client.write "\x1B\x21\x30 2x width, height... 你好啊\n"
 
-puts "=== 666"
+content = "你好，我是第三行"
+puts "== encoding: #{content.encoding}"
+
+content = content.encode 'gbk', 'utf-8'
+puts "== after , encoding: #{content.encoding}"
+
+client.write content
+
 client.write "\n\n\n\n\n\n\n\n"
 
-#client.write "\x1B\x40"
 client.write "\x1D\x56\x01"
-
 sleep 0.1
 
 client.close
